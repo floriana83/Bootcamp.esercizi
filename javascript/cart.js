e05934a5322c593b5a13ed052ce12af2bfa3db7f
Floriana Domianello <floriana.domianello@gmail.com>
2021-10-21 19:53:04 +0200

    find duplicate

--- javascript/cart.js
+++ javascript/cart.js
@@ -5,13 +5,22 @@ function mioCarrello() {
     let prodotto = prompt ('Scegli i tuoi prodotti');
     let scelta = prodotto.split (' ');
 
-    if (prodotto === "") {
+    if (prodotto === null || prodotto === "") {
         alert ("hai dimenticato di inserire un prodotto");
     } 
-
     
+    if (cart.includes(prodotto) == true) {
+        alert ( "hai inserito questo prodotto:" + prodotto );
+    } else {
+        alert ("prodotto già inserito: " + prodotto);
+    }
+
+    cart.push(prodotto);
+    console.log (cart);
+
+    return prodotto;
 } 
 
-console.log (cart);
+
 
 
